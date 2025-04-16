7ff579e552ac771ed8d4aff894b6ca24e0c867dc
noahvogt1 <96090888+noahvogt1@users.noreply.github.com>
2025-04-16 14:14:05 -0600

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -2,5 +2,8 @@ const fs = require('fs');
 const assert = require('assert');
 eval(fs.readFileSync('code.js')+'');
 
-assert(factorial(10) === 3628800);
+assert(e(1) === 2);
+assert(e(4) === 2.708333333333333);
+assert(e(8) === 2.71827876984127);
+assert(e(9) === 2.7182815255731922);
 assert(e(10) === 2.7182818011463845);
